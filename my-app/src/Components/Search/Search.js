038d49cc109a657080ae9500910af40bd16e3524
yosef sisero <yosefsisero@gmail.com>
2020-07-09 21:07:21 -0300

    avanzamos en el archivo search

--- my-app/src/Components/Search/Search.js
+++ my-app/src/Components/Search/Search.js
@@ -9,60 +9,81 @@ function Search() {
     const [genero, setGenero] = useState ('');
     const [resum, setResum] = useState ('');
     const [album, setAlbum] = useState ('');
-    
+    const [track, setTrack] = useState ('');
+    const [image, setImage] = useState ('');
+    const [musica, setMusica] = useState ('')
 
     const API = `http://ws.audioscrobbler.com/2.0/`;
     const KEY = 'ebbf1095239b4faa07ac8e41009d2575';
     const URLINFO = `${API}?method=artist.getinfo&artist=Cher&api_key=${KEY}&format=json`;
     const URLALBUM = `${API}?method=artist.gettopalbums&artist=cher&api_key=${KEY}&format=json`;
+    const URLTRACK = `${API}?method=artist.gettoptracks&artist=cher&api_key=${KEY}&format=json`;
 
-    const getinfo = () =>{
-        axios.get (URLINFO)
-        .then((response) => {
-         let nombre = response.data.artist.name
-         let estilo = response.data.artist.tags.tag[0].name
-         let resumen = response.data.artist.bio.summary
-
-           setArtista(nombre)
-           setGenero(estilo)
-           setResum(resumen)
-          
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-
-        };  
+    useEffect(()=> {
+            axios.get (URLINFO)
+            .then((response) => {
+             let nombre = response.data.artist.name
+             let estilo = response.data.artist.tags.tag[0].name
+             let resumen = response.data.artist.bio.summary
+    
+               setArtista(nombre)
+               setGenero(estilo)
+               setResum(resumen)
+              
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    
+              
+    }, [])
         
-        getinfo()
 
-            
+    useEffect(()=> {
+       
+   axios.get (URLALBUM)
+      .then((response) => {
+       let album1 = response.data.topalbums.album[0].name
+       let foto1 = response.data.topalbums.album[0].image[1]['#text']
 
-        // const getalbum = () =>{
-        //     axios.get (URLALBUM)
-        //     .then((response) => {
-        //      let album1 = response.data.topalbums.album[0].name
-             
+      setAlbum(album1)
+      setImage(foto1)
+                  
+    })
+     .catch((error) => {
+     console.log(error);
+     })        
+}, [])
+
+useEffect(()=> {
+       
+    axios.get (URLTRACK)
+    .then((response) => {
+     let track1 = response.data.toptracks.track[0].name
+     let track2 = response.data.toptracks.track[0].url
     
-        //        setAlbum(album1)
-              
-              
-        //     })
-        //     .catch((error) => {
-        //         console.log(error);
-        //     })
-    
-        //     };  
-            
-        //     getinfo()
+       
+       setTrack(track1)
+       setMusica(track2)
+     
+     
+    })
+    .catch((error) => {
+        console.log(error);
+    })        
+}, [])
 
 
     return (
         <>
+
                <p> Artist: {artista}</p> 
                <p> Gen: {genero}</p>
                <p> Summary: {resum}</p>
-               <p> Summary: {album}</p>
+               <p> Album: {album}</p>
+               <img src={image}/>
+               <p> Track: {track}</p>
+               <p>sonido: {musica}</p>
         </>
     )
 }
