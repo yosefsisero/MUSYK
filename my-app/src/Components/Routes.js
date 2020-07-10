76c2f380527a81b68257ce54a5f0a746c2edb3d5
yosef sisero <yosefsisero@gmail.com>
2020-07-10 14:58:44 -0300

    avanzamos en el proyecto

--- my-app/src/Components/Routes.js
+++ my-app/src/Components/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Top10 from './Pages/Top10/Top10'
+import Buscar from './Pages/Buscar/Buscar'
 
 
 function Routes() {
@@ -11,6 +12,7 @@ function Routes() {
            <Switch>
                <Route exact path="/" component={Home}/>
                <Route exact path="/top10" component={Top10}/>
+               <Route exact path="/buscar" component={Buscar}/>
            </Switch>
         </BrowserRouter>
         </>
