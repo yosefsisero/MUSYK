5f3b7cfbbfa0a7534b2e099a787f1af6cfaffcc4
Jacobo Zonana <jacobozonana@gmail.com>
2020-07-11 23:27:07 -0500

    se hacen alunas cosas del footer

--- my-app/src/Components/Layout/Footer/Footer.js
+++ my-app/src/Components/Layout/Footer/Footer.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import './Footer.css'
+import Logo from '../Header/Logo.png'
+// import '../../../../node_modules/font-awesome/css/font-awesome.min.css'; 
+
+
+
+
 
 function Footer() {
     return (
         <>
-            <p>Este es el footer</p>
+        <footer className="footer">
+        
+        <img className="logo" src={Logo}/>
+        
+        <p>© Yosef Sisero / Jacobo Zonana 2020 ® Musyk All Rigths Reserved</p>
+        
+        {/* <div className="redes">
+           
+           <i><a className="fab fa-facebook-square fa-2x" href="https://www.facebook.com/></a></i>
+           <i><a className="fab fa-twitter fa-2x" href="https://twitter.com/"></a></i>
+           <i><a className="fab fa-instagram fa-2x" href="https://www.instagram.com/"></a></i>
+           <i><a className="fab fa-pinterest fa-2x" href="https://ar.pinterest.com/"></a></i>
+           <i><a className="fab fa-linkedin fa-2x" href="https://www.linkedin.com/"></a></i>
+       
+        </div> */}
+        
+
+
+        </footer>  
+
+    
+            
         </>
     )
 }
