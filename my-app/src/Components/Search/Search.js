945022e85690bc997f4cd4e79f2e997db5a62ee6
Jacobo Zonana <jacobozonana@gmail.com>
2020-07-14 22:09:17 -0500

    se logro poner el boton de la musica y que suene lo que se busca :collision: :exclamation:

--- my-app/src/Components/Search/Search.js
+++ my-app/src/Components/Search/Search.js
@@ -9,16 +9,23 @@ function Search() {
     const [genero, setGenero] = useState ([]);
     const [resum, setResum] = useState ('');
     const [album, setAlbum] = useState ([]);
-    const [track, setTrack] = useState ([]);
-    const [musica, setMusica] = useState ([])
+    // const [track, setTrack] = useState ([]);
+    // const [musica, setMusica] = useState ([])
     const [similar, setSimilar] = useState([])
     const [toFind, setToFind] = useState('');
+    const [id, setId] = useState ('');
+    const [tr, setTr] = useState ('');
+    const [nm,setNm] = useState ('');
 
     const API = `http://ws.audioscrobbler.com/2.0/`;
     const KEY = 'ebbf1095239b4faa07ac8e41009d2575';
+    const NAPKEY = `ZmU4YzdlNTAtNDVjNy00NmRjLTkxODItNDJmZTJhNDE1ZjQw`;
     const URLINFO = `${API}?method=artist.getinfo&artist=${toFind}&api_key=${KEY}&format=json&limit=5`;
     const URLALBUM = `${API}?method=artist.gettopalbums&artist=${toFind}&api_key=${KEY}&format=json&limit=5`;
-    const URLTRACK = `${API}?method=artist.gettoptracks&artist=${toFind}&api_key=${KEY}&format=json&limit=10`;
+    // const URLTRACK = `${API}?method=artist.gettoptracks&artist=${toFind}&api_key=${KEY}&format=json&limit=10`;
+    const NAPURLINFO = `https://api.napster.com/v2.1/artists/${toFind}?apikey=${NAPKEY}&limit=5`;
+    const NAPTRACK = `https://api.napster.com/v2.1/artists/${id}/tracks/top?apikey=${NAPKEY}&limit=5`
+    
 
     useEffect(()=> {
             axios.get (URLINFO)
@@ -56,23 +63,57 @@ function Search() {
      })        
 }, [toFind])
 
-useEffect(()=> {
+// useEffect(()=> {
        
-    axios.get (URLTRACK)
-    .then((response) => {
-     let track1 = response.data.toptracks.track
-     let track2 = response.data.toptracks.track[0].url
+//     axios.get (URLTRACK)
+//     .then((response) => {
+//      let track1 = response.data.toptracks.track
+//      let track2 = response.data.toptracks.track[0].url
     
        
-       setTrack(track1)
-       setMusica(track2)
+//        setTrack(track1)
+//        setMusica(track2)
      
      
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })        
+// }, [toFind])
+
+useEffect(()=> {
+    axios.get (NAPURLINFO)
+    .then((response) => {
+     let id1 = response.data.artists[0].id
+     
+
+       setId(id1)
+       console.log(id1)
     })
     .catch((error) => {
         console.log(error);
-    })        
+    })
+
+      
 }, [toFind])
+
+useEffect(()=> {
+    axios.get (NAPTRACK)
+    .then((response) => {
+     let tr1 = response.data.tracks[0].previewURL
+     let nm1 = response.data.tracks[0].name
+     
+
+       setTr(tr1)
+       setNm(nm1)
+       
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+      
+}, [id])
 
 
 
@@ -83,14 +124,14 @@ useEffect(()=> {
           <input placeholder=" Search Artist" onChange={(event)=>{setToFind(event.target.value)}}/>
           <button onClick={(event)=>{setToFind(event.target.value)}}>Search</button>  
                <p> Artist: {artista}</p> 
-               
+               <p><audio controls src={tr} ></audio>{nm}</p>
                <p> Gen: {genero.map((gen) =>{
                    return <p>{gen.name}</p>
                })}</p>
 
                <p> Summary: {resum}</p>
 
-               <p> Album: {
+               <p> Top Albums: {
                     album.map((alb)=>{
                         return <div>
                             <p>{alb.name}</p>
@@ -99,15 +140,19 @@ useEffect(()=> {
                     })
                     }</p>
 
-               <p> Track: {track.map((tra) =>{
+
+                    
+
+               {/* <p> Track: {track.map((tra) =>{
                    return <p>{tra.name}</p>
-               })}</p>
+               })}</p> */}
 
-               <iframe src={musica}/>
+               {/* <iframe src={musica}/> */}
 
-               <p>sonido: {musica}</p>
+               
 
-               <p>Similar: {similar.map((sim) => {
+
+               <p>Similar Artists: {similar.map((sim) => {
                    return <div>
                        <p>{sim.name}</p>
                        </div>
@@ -122,7 +167,6 @@ export default Search
 /*{ PENDIENTES
     
     CON DAVID
-    2: como escuchar la musica 
     2:hacer el input para buscar con boton
 
     NUESTROS
